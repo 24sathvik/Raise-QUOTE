8e49c60aba8075f48ff15d2045566aaffd32fab8
Nishanth-dev069 <nishanthpallay@gmail.com>
2026-03-03 20:49:12 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/admin/:path*',
-    '/auth/:path*',
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
